c51919cae9a74247cc66c064a367a65b7075961d
Andrey Zaklitskiy <glockman94@gmail.com>
2018-01-25 16:55:35 +0200

    site is done

--- app/scripts/humburger.js
+++ app/scripts/humburger.js
@@ -1,23 +1,7 @@
-/*
-$(document).ready(function() {
-  var burger = $('.humburger');
-  var burger-active = $('.is-active');
-
-  burger.click(function () {
-
-  })
-
-});
-
-*/
-
-
 $(document).ready(function() {
   $('.hamburger').click(function () {
     $('.hamburger').toggleClass('is-active');
     $('.hamburger-menu').toggleClass('visible');
-
-
   });
 });
 
